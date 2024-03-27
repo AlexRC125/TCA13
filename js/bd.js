3b2e3a2c9f5a598c070349277ae03b1354ed77f8
Alex <125aramoscortes@frhsd.com>
2024-03-26 22:39:11 -0400

    first commit

---
+++ js/bd.js
@@ -0,0 +1,19 @@
+function updateSix(){
+    document.getElementById('output').innerHTML = "August 27th, 2007 marks not only the celebration of my birthday but also a date intertwined with significant events throughout history. From groundbreaking discoveries to pivotal moments in culture and politics, August 27th stands as a day of both personal significance and broader historical resonance. As I reflect on another year passing, I am reminded of the fascinating tapestry of events that have unfolded on this day, shaping the world in many ways.";
+    document.getElementById('img').src="img/calendar.png";
+}
+function updateTwo(){
+    document.getElementById('output').innerHTML = "On August 27, 1859, the Drake Well struck oil at 69½ feet, while drilling in Titusville, Pennsylvania, becoming the first driller of a productive oil well in the United States.";
+    document.getElementById('img').src="img/oil3.jpg";
+}
+function updateThree(){
+    document.getElementById('output').innerHTML = "The Kellogg-Briand Pact was an agreement to outlaw war signed on August 27, 1928. Sometimes called the Pact of Paris for the city in which it was signed, the pact was one of many international efforts to prevent another World War, but it had little effect in stopping the rising militarism of the 1930s or preventing World War II.";
+    document.getElementById('img').src="img/pact2.webp";
+}
+function updateFour(){
+    document.getElementById('output').innerHTML = "A young German physicist, Hans von Ohain, worked for Ernst Heinkel, specializing in advanced engines, to develop the world's first jet plane, the experimental Heinkel He 178. I was the first jet-powered aircraft, it flew on Aug. 27, 1939, nearly two years before its British equivalent, the Gloster E. 28/39";
+    document.getElementById('img').src="img/plane1.webp";
+}
+
+
+
